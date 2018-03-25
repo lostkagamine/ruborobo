1882ba462e94158326022e80234a2c789ca5dbd2
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-03-25 19:59:31 +0100

    add sh

--- plugins/admin.rb
+++ plugins/admin.rb
@@ -25,6 +25,21 @@ module Ruborobo
         end
     end
 
+    $bot.cmd(:shell, perms:[:bot_owner], desc:'runs a shell command', invokers:[:sh]) do |ev, args|
+        begin
+            r = `#{args.raw.join(' ')}`
+            if r.length > 1984
+                hbresp = RestClient.post('https://hastebin.com/documents', r)
+                thing = eval hbresp.body
+                ":warning: Output too long! (#{res.length} > 1984 characters) See the full output here: https://hastebin.com/raw/#{thing[:key]}"
+            else
+                "```#{r}```"
+            end
+        rescue Exception => e
+            "```\n#{e}```"
+        end
+    end
+
     $bot.cmd(:echo, perms:[:bot_owner], desc:'it says what you say', invokers:[:say]) do |ev, args|
         args.raw.join(' ')
     end
